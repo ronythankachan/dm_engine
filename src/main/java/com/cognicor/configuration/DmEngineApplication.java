d0e99ca435d7fc42165cb2647c4e6b0204abd87a
rony thankachan <rony.mail2me@gmail.com>
2023-07-25 14:40:03 +0530

    Project created

---
+++ src/main/java/com/cognicor/configuration/DmEngineApplication.java
@@ -0,0 +1,13 @@
+package com.cognicor.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognicor/configuration/DmEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognicor.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
